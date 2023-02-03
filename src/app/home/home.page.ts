dfe2ff067191ff18b79b0a5f3c8dce3b2e8c0963
Nelson Morera <workemailnelson@gmail.com>
2023-02-02 20:46:11 -0600

    20230202v1.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
+pokemonsArray = [
+{
+  name :'Charmander',
+  url : '../assets/icons/charmander.png'
+},
+{
+  name :'Pikachu',
+  url : '../assets/icons/pikachu.png'
+},
+{
+  name :'Bullbasaur',
+  url : '../assets/icons/bullbasaur.png'
+}
 
+];
   constructor() {}
 
+  ngOnInit() {
+    
+  }
 }
